f0a73f390bdb47eda45d99dfa93d670f544ab502
Yuşa <yusasarisoy@icloud.com>
2025-10-03 15:07:49 +0300

    Add unit test for BKAxis

---
+++ Tests/BindKitTests/Types/BKAxisTests.swift
@@ -0,0 +1,35 @@
+//
+//  BKAxisTests.swift
+//  BindKit
+//
+//  Created by Yuşa on 3.10.2025.
+//
+
+import Testing
+@testable import BindKit
+
+@Suite("BKAxis")
+struct BKAxisTests {
+    
+    // MARK: - Tests
+
+    @Test
+    func allCases_exist_andSwitchWorks() {
+        let axes: [BKAxis] = [
+            .horizontal,
+            .vertical,
+            .both
+        ]
+        
+        for axis in axes {
+            switch axis {
+            case .horizontal:
+                #expect(axis == .horizontal)
+            case .vertical:
+                #expect(axis == .vertical)
+            case .both:
+                #expect(axis == .both)
+            }
+        }
+    }
+}
